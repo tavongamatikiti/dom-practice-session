b28b13592e735b330dbbff8e80c44452ffdfd7cc
Noel Odero <anoel.odero@gmail.com>
2024-12-09 11:56:47 +0200

    word styling

--- cloud.js
+++ cloud.js
@@ -1,6 +1,6 @@
 const myParagraph = document.getElementById("myParagraph").textContent;
 // console.log(myParagraph)
-const newParagraph = myParagraph.replace(/[,:;.\n]/g, '')
+const newParagraph = myParagraph.toLowerCase().replace(/[,:;.\n]/g, '')
 // console.log(newParagraph)
 const newWords = newParagraph.trim().split(' ')
 // console.log(newWords);
@@ -17,4 +17,16 @@ const sortedWords = Object.entries(wordCount).sort((a,b) => b[1] - a[1])
 const filteredWords = sortedWords.filter(n => n[0] !== "")
 console.log(filteredWords);
 
-// const 
+
+const topWords = sortedWords.slice(0, 12);
+
+//  console.log(topWords);
+    const wordCloud = document.getElementById('myWordCloud');
+    topWords.forEach((word, index) => {
+        const span = document.createElement('span');
+        span.textContent = word[0];
+        span.style.fontSize = `${64 - index * 4}px`;
+        wordCloud.appendChild(span);
+        wordCloud.appendChild(document.createTextNode(' '));
+    });
+
